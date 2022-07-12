6520d5a1a763c92df66b08d55467e26718e9db1a
illusies <john_illusies@yahoo.com>
2022-07-12 07:50:15 -0700

    Corrected syntax

--- parser.c
+++ parser.c
@@ -1,23 +1,21 @@
 #include "main.h"
 
 /**
- * parser - Receives the main string and all the necessary parameters to
- * print a formated string.
- * @format: A string containing all the desired characters.
- * @f_list: A list of all the posible functions.
- * @arg_list: A list containing all the argumentents passed to the program.
- * Return: A total count of the characters printed.
+ * parser
+ * @format: const char
+ * @f_list: list
+ * @arg_list: va_list
+ * Return: (0)
  */
 int parser(const char *format, conver_t f_list[], va_list arg_list)
 {
 	int i, j, r_val, printed_chars;
 
 	printed_chars = 0;
-	for (i = 0; format[i] != '\0'; i++)/* Iterates through the main str*/
+	for (i = 0; format[i] != '\0'; i++)
 	{
-		if (format[i] == '%') /*Checks for format specifiers*/
+		if (format[i] == '%')
 		{
-			/*Iterates through struct to find the right func*/
 			for (j = 0; f_list[j].sym != NULL; j++)
 			{
 				if (format[i + 1] == f_list[j].sym[0])
@@ -40,13 +38,14 @@ int parser(const char *format, conver_t f_list[], va_list arg_list)
 				else
 					return (-1);
 			}
-			i = i + 1; /*Updating i to skip format symbols*/
+			i = i + 1;
 		}
 		else
 		{
-			_write_char(format[i]); /*call the write function*/
+			_write_char(format[i]);
 			printed_chars++;
 		}
 	}
 	return (printed_chars);
 }
+
