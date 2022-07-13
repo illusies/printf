c7ad048ac8d6d6761c165724aad47328e83b514f
illusies <john_illusies@yahoo.com>
2022-07-12 17:34:56 -0700

    Incremental Updates

--- main.h
+++ main.h
@@ -17,7 +17,6 @@ struct convert
 };
 typedef struct convert conver_t;
 
-/*Main functions*/
 int parser(const char *format, conver_t f_list[], va_list arg_list);
 int _printf(const char *format, ...);
 int _write_char(char);
@@ -33,8 +32,6 @@ int unsigned_integer(va_list);
 int print_octal(va_list list);
 int print_hex(va_list list);
 int print_heX(va_list list);
-
-/*Helper functions*/
 unsigned int base_len(unsigned int, int);
 char *rev_string(char *);
 void write_base(char *str);
@@ -42,3 +39,4 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 int print_unsgined_number(unsigned int);
 
 #endif
+
